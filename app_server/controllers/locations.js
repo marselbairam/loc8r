cacfe0f26d4ab54c5edd00febc960ed706a5f712
Marsel <marselbairam@gmail.com>
2020-05-07 21:29:00 +0300

    get location info data from db

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -73,44 +73,45 @@ module.exports.homelist = function(req, res) {
   );
 };
 
-module.exports.locationInfo = function(req, res) {
+var renderDetailPage = function(req, res, locDetail) {
   res.render('location-info', {
-    title: 'Starcups',
+    title: locDetail.name,
     pageHeader: {
-      title: 'Starcups'
+      title: locDetail.name
     },
     sidebar: {
       context: 'Simon\'s cafe is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
       callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
     },
-    location: {
-      name: 'Starcups',
-      address: '125 High Street, Reading, RG6 1PS',
-      rating: 3,
-      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-      coords: {lat: -0.9690884, lng: 51.455041},
-      openingTimes: [{
-        days: 'Monday - Friday',
-        opening: '7:00am',
-        closing: '7:00pm',
-        closed: false
-      }, {
-        days: 'Saturday',
-        opening: '8:00am',
-        closing: '5:00pm',
-        closed: false
-      }, {
-        days: 'Sunday',
-        closed: true
-      }],
-      reviews: [{
-        author: 'Simon Holmes',
-        rating: 5,
-        timestamp: '16 May 2020',
-        reviewText: 'What a great place. I can\'t say enough good things about it.'
-      }]
-    }
+    location: locDetail
   });
+};
+
+module.exports.locationInfo = function(req, res) {
+  var requestOptions, path;
+  path = '/api/locations/' + req.params.locationid;
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: 'GET',
+    json: {},
+    qs: {
+      lng: -0.9693959,
+      lat: 51.452039,
+      maxDistance: 5000
+    }
+  };
+  request(
+    requestOptions,
+    function(err, response, body) {
+      var data = body;
+      data.coords = {
+        lng: body.coords[0],
+        lat: body.coords[1]
+      };
+      renderDetailPage(req, res, data);
+      console.log(body);
+    }
+  );
 };
 
 module.exports.addReview = function(req, res) {
