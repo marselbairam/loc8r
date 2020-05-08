d5d2dad46f05574272707531a9b3620a3bf30591
Marsel <marselbairam@gmail.com>
2020-05-08 08:41:19 +0300

    create showError function and add error handling to locationInfo controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -23,6 +23,22 @@ var _formatDistance = function(distance) {
   }
 };
 
+var _showError = function(req, res, status) {
+  var title, content;
+  if (status === 404) {
+    title = '404, page not found';
+    content = 'Oh dear. Looks like we can\'t find this page. Sorry.';
+  } else {
+    title = status + ', something\'s gone wrong';
+    content = 'Something, somewhere, has gone just a little bit wrong.';
+  }
+  res.status(status);
+  res.render('generic-text', {
+    title: title,
+    content: content
+  });
+};
+
 var renderHomepage = function(req, res, responseBody) {
   var message;
   if (!(responseBody instanceof Array)) {
@@ -104,12 +120,15 @@ module.exports.locationInfo = function(req, res) {
     requestOptions,
     function(err, response, body) {
       var data = body;
-      data.coords = {
-        lng: body.coords[0],
-        lat: body.coords[1]
-      };
-      renderDetailPage(req, res, data);
-      console.log(body);
+      if (response.statusCode === 200) {
+        data.coords = {
+          lng: body.coords[0],
+          lat: body.coords[1]
+        };
+        renderDetailPage(req, res, data);
+      } else {
+        _showError(req, res, response.statusCode);
+      }
     }
   );
 };
