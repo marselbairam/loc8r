11a12204cc4de99cfb465d371f709307b1effb7f
Marsel <marselbairam@gmail.com>
2020-05-13 16:57:28 +0300

    add default layout controller

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -4,3 +4,7 @@ module.exports.about = function(req, res) {
     content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.'
   });
 };
+
+module.exports.angularApp = function(req, res) {
+  res.render('layout', { title: 'Loc8r' });
+};
