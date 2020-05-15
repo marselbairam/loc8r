ac2fec972e6730488107656b637a5cf1d6c84962
Marsel <marselbairam@gmail.com>
2020-05-15 08:21:16 +0300

    add dependecy injection and replace template string

--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -3,9 +3,10 @@
     .module('loc8rApp')
     .service('loc8rData', loc8rData);
 
+  loc8rData.$inject = ['$http'];
   function loc8rData($http) {
     var locationByCoords = function(lat, lng) {
-      return $http.get(`/api/locations?lng=${lng}&lat=${lat}&maxDistance=5`);
+      return $http.get('/api/locations?lng=' + lng + '&lat=' + lat + '&maxDistance=5');
     };
     return {
       locationByCoords: locationByCoords
