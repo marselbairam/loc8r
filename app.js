5d5aab2a15703a9bce7cedfc1f2bf91b0694cf03
Marsel <marselbairam@gmail.com>
2020-05-15 08:21:42 +0300

    uglify js files

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var uglifyJs = require('uglify-js');
+var fs = require('fs');
 require('./app_api/models/db');
 
 var indexRouter = require('./app_server/routes/index');
@@ -13,6 +15,25 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
+
+var appClientFiles = {
+  'app': fs.readFileSync('app_client/app.js', 'utf-8'),
+  'home': fs.readFileSync('app_client/home/home.controller.js', 'utf-8'),
+  'geolocation': fs.readFileSync('app_client/common/services/geolocation.service.js', 'utf-8'),
+  'loc8rData': fs.readFileSync('app_client/common/services/loc8rData.service.js', 'utf-8'),
+  'formatDistance': fs.readFileSync('app_client/common/filters/formatDistance.filter.js', 'utf-8'),
+  'ratingStars': fs.readFileSync('app_client/common/directives/ratingStars/ratingStars.directive.js', 'utf-8')
+};
+
+var uglified = uglifyJs.minify(appClientFiles, { compress: false });
+
+fs.writeFile('public/angular/loc8r.min.js', uglified.code, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Script generated and saved: loc8r.min.js');
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
