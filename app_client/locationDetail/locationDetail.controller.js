7ff8dc21abc7960a8bb830ade2f0d75162e01620
Marsel <marselbairam@gmail.com>
2020-05-18 17:00:10 +0300

    upd locationDetail

--- app_client/locationDetail/locationDetail.controller.js
+++ app_client/locationDetail/locationDetail.controller.js
@@ -3,10 +3,27 @@
     .module('loc8rApp')
     .controller('locationDetailCtrl', locationDetailCtrl);
 
-  function locationDetailCtrl() {
+  locationDetailCtrl.$inject = ['$routeParams', '$modal', 'loc8rData'];
+  function locationDetailCtrl($routeParams, $modal, loc8rData) {
     var vm = this;
-    vm.pageHeader = {
-      title: 'Location detail page'
+    vm.locationid = $routeParams.locationid;
+
+    loc8rData.locationById(vm.locationid)
+      .success(function(data) {
+        vm.data = { location: data };
+        vm.pageHeader = {
+          title: vm.data.location.name
+        };
+      })
+      .error(function(e) {
+        console.log(e);
+      });
+
+    vm.popupReviewForm = function() {
+      var modalInstance = $modal.open({
+        templateUrl: '/reviewModal/reviewModal.view.html',
+        controller: 'reviewModalCtrlasvm'
+      });
     };
   }
 })();
